67bc2daf45df2d2e1be7c8cf0fcdd3348bc01114
liang1127 <sam8711276204@gmail.com>
2018-05-18 17:25:46 +0800

    hw2.c

--- hw2.c
+++ hw2.c
@@ -7,27 +7,27 @@ int main(int argc, char *argv[])
 {
 		int theSeason;
 	
-	printf("請輸入月份(1~12)\n");
-	scanf("%d",&theSeason);
+	printf("請輸入月份(1~12)\n");					
+	scanf("%d",&theSeason);								
 	
-	if(theSeason>=3&theSeason<=5)
+	if(theSeason>=3&theSeason<=5)							//判斷是否是春天 
 	{
 		printf("春天來瞭!");
 	}
-	else if(theSeason>=6&theSeason<=8)
+	else if(theSeason>=6&theSeason<=8)						//判斷是否是夏天 
 	{
 		printf("男人的季節!");
 	}
-	else if(theSeason>=9&theSeason<=11)
+	else if(theSeason>=9&theSeason<=11)						//判斷是否是秋天 
 	{
 		printf("秋天");
 	}
-	else if (theSeason==12|theSeason==1|theSeason==2)
+	else if (theSeason==12|theSeason==1|theSeason==2)		//判斷是否是冬天	
 	{
 		printf("冬天");
 		
 	}
-	else if (theSeason>=13)
+	else if (theSeason>=13)									//判斷是否是正確的月份 
 	{
 		printf("輸入錯誤");
 	}
